684139ed581907aea986621c550c5bd205621066
noahisaac1337 <axaxa.azaza2001@gmail.com>
2021-01-24 23:09:39 +0500

    Вход

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        $emp = DB::table('employees')->pluck('Id');
+        $empcount = count($emp);
+
+        $role = DB::table('role')->pluck('Id');
+        $rolecount = count($role);
+
+        return view('auth.register',['empcount' => $empcount],['rolecount' => $rolecount]);
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'login' => 'required|string|max:255',
+            'empid' =>'required|int',
+            'roleid' =>'required|int',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        Auth::login($user = User::create([
+            'login' => $request->login,
+            'empid' => $request->empid,
+            'roleid' => $request->roleid,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]));
+
+        event(new Registered($user));
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
